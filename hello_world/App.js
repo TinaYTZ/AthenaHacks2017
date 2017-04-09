e19e0e2cb09c0eefb42a48a9ac35efb7f00ff96e
Yuting Zhang <tinaytz@users.noreply.github.com>
2017-04-09 05:48:40 -0700

    reactive native app

--- hello_world/App.js
+++ hello_world/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+
+import { StyleSheet, Text, View , Button} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.title}>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-
-      <Button
-       OnPress{()=> alert('hello world')}
-       title="click me"
-       />
-       </View>
+        <Button
+             onPress={()=> alert('hello world!')}
+             title="click me"
+             />
+      </View>
     );
   }
 }
@@ -24,5 +24,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title:{
+    fontSize: 30,
   },
 });
